ef1f53d47b5bbbeb2f44f88ff71f20d642dd6230
Rockhount <rocktest01@web.de>
2016-06-20 18:52:53 +0200

    #1
    Initial sourcecode upload

---
+++ Visual Studio 2015 Project (Sourcecode) - SQMCleaner/SQMCleaner/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace SQMCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PathLabel = new System.Windows.Forms.Label();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.PathButton = new System.Windows.Forms.Button();
+            this.SimpleDeleteLabel = new System.Windows.Forms.Label();
+            this.SimpleDeleteTextBox = new System.Windows.Forms.TextBox();
+            this.CleanButton = new System.Windows.Forms.Button();
+            this.DeleteAllCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // PathLabel
+            // 
+            this.PathLabel.AutoSize = true;
+            this.PathLabel.Location = new System.Drawing.Point(13, 22);
+            this.PathLabel.Name = "PathLabel";
+            this.PathLabel.Size = new System.Drawing.Size(32, 13);
+            this.PathLabel.TabIndex = 0;
+            this.PathLabel.Text = "Path:";
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.BackColor = System.Drawing.Color.White;
+            this.PathTextBox.Location = new System.Drawing.Point(51, 19);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.ReadOnly = true;
+            this.PathTextBox.Size = new System.Drawing.Size(309, 20);
+            this.PathTextBox.TabIndex = 1;
+            // 
+            // PathButton
+            // 
+            this.PathButton.Location = new System.Drawing.Point(366, 17);
+            this.PathButton.Name = "PathButton";
+            this.PathButton.Size = new System.Drawing.Size(75, 23);
+            this.PathButton.TabIndex = 2;
+            this.PathButton.Text = "Search";
+            this.PathButton.UseVisualStyleBackColor = true;
+            this.PathButton.Click += new System.EventHandler(this.PathButton_Click);
+            // 
+            // SimpleDeleteLabel
+            // 
+            this.SimpleDeleteLabel.AutoSize = true;
+            this.SimpleDeleteLabel.Location = new System.Drawing.Point(16, 64);
+            this.SimpleDeleteLabel.Name = "SimpleDeleteLabel";
+            this.SimpleDeleteLabel.Size = new System.Drawing.Size(200, 13);
+            this.SimpleDeleteLabel.TabIndex = 3;
+            this.SimpleDeleteLabel.Text = "Delete CustomAttributes, which contains:";
+            // 
+            // SimpleDeleteTextBox
+            // 
+            this.SimpleDeleteTextBox.Location = new System.Drawing.Point(19, 80);
+            this.SimpleDeleteTextBox.Name = "SimpleDeleteTextBox";
+            this.SimpleDeleteTextBox.Size = new System.Drawing.Size(341, 20);
+            this.SimpleDeleteTextBox.TabIndex = 4;
+            // 
+            // CleanButton
+            // 
+            this.CleanButton.Enabled = false;
+            this.CleanButton.Location = new System.Drawing.Point(366, 78);
+            this.CleanButton.Name = "CleanButton";
+            this.CleanButton.Size = new System.Drawing.Size(75, 23);
+            this.CleanButton.TabIndex = 7;
+            this.CleanButton.Text = "Clean";
+            this.CleanButton.UseVisualStyleBackColor = true;
+            this.CleanButton.Click += new System.EventHandler(this.CleanButton_Click);
+            // 
+            // DeleteAllCheckBox
+            // 
+            this.DeleteAllCheckBox.AutoSize = true;
+            this.DeleteAllCheckBox.Location = new System.Drawing.Point(290, 63);
+            this.DeleteAllCheckBox.Name = "DeleteAllCheckBox";
+            this.DeleteAllCheckBox.Size = new System.Drawing.Size(70, 17);
+            this.DeleteAllCheckBox.TabIndex = 8;
+            this.DeleteAllCheckBox.Text = "Delete all";
+            this.DeleteAllCheckBox.UseVisualStyleBackColor = true;
+            this.DeleteAllCheckBox.CheckedChanged += new System.EventHandler(this.DeleteAllCheckBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 108);
+            this.Controls.Add(this.DeleteAllCheckBox);
+            this.Controls.Add(this.CleanButton);
+            this.Controls.Add(this.SimpleDeleteTextBox);
+            this.Controls.Add(this.SimpleDeleteLabel);
+            this.Controls.Add(this.PathButton);
+            this.Controls.Add(this.PathTextBox);
+            this.Controls.Add(this.PathLabel);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "SQM Cleaner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PathLabel;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Button PathButton;
+        private System.Windows.Forms.Label SimpleDeleteLabel;
+        private System.Windows.Forms.TextBox SimpleDeleteTextBox;
+        private System.Windows.Forms.Button CleanButton;
+        private System.Windows.Forms.CheckBox DeleteAllCheckBox;
+    }
+}
+
---
+++ Visual Studio 2015 Project (Sourcecode) - SQMCleaner/SQMCleaner/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace SQMCleaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string[] RAWTextLines { get; set; }
+
+        private void PathButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var openSQMDialog = new OpenFileDialog();
+                openSQMDialog.Title = "Select SQM File:";
+                openSQMDialog.Filter = "SQM File|*.sqm";
+                openSQMDialog.InitialDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                openSQMDialog.Multiselect = false;
+                var DialogResult = openSQMDialog.ShowDialog(this);
+                if (DialogResult == DialogResult.OK)
+                {
+                    PathTextBox.Text = openSQMDialog.FileName;
+                    CleanButton.Enabled = true;
+                }
+                else
+                {
+                    CleanButton.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private void CleanButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                PathButton.Enabled = false;
+                DeleteAllCheckBox.Enabled = false;
+                CleanButton.Enabled = false;
+                if (!DeleteAllCheckBox.Checked)
+                {
+                    SimpleDeleteTextBox.Enabled = false;
+                }
+
+                if (File.Exists(PathTextBox.Text))
+                {
+                    RAWTextLines = File.ReadAllLines(PathTextBox.Text);
+                    var NextCustomAttributeIndex = GetTextLineIndex(0, "class CustomAttributes", string.Empty);
+                    var NextAttributeIndex = 0;
+                    var NextExpressionIndex = 0;
+                    var DeleteFromIndex = 0;
+                    var DeleteToIndex = -1;
+                    while (NextCustomAttributeIndex > -1)
+                    {
+                        if (!DeleteAllCheckBox.Checked)
+                        {
+                            NextAttributeIndex = GetTextLineIndex(NextCustomAttributeIndex, "class Attribute", "nAttributes=");
+                            while (NextAttributeIndex > -1)
+                            {
+                                NextExpressionIndex = GetTextLineIndex(NextAttributeIndex, "expression=", string.Empty);
+                                if (RAWTextLines[NextExpressionIndex].ToLowerInvariant().Contains(SimpleDeleteTextBox.Text.ToLowerInvariant()))
+                                {
+                                    DeleteFromIndex = NextAttributeIndex;
+                                    DeleteToIndex = GetTextLineIndex(NextExpressionIndex, "class Attribute", "nAttributes=");
+                                    if (DeleteToIndex == -1)
+                                    {
+                                        DeleteToIndex = GetTextLineIndex(NextExpressionIndex, "nAttributes=", string.Empty);
+                                    }
+                                    CleanUpLines(DeleteFromIndex, DeleteToIndex - 1);
+                                }
+                                NextAttributeIndex = GetTextLineIndex(NextExpressionIndex, "class Attribute", "nAttributes=");
+                            }
+                            var CurrentRawTextLines = RAWTextLines.ToList().GetRange(NextCustomAttributeIndex, NextExpressionIndex - NextCustomAttributeIndex);
+                            var LeftAttributeCount = CurrentRawTextLines.Where(TextLine => TextLine.ToLowerInvariant().Contains("class Attribute".ToLowerInvariant())).Count();
+                            if (LeftAttributeCount == 0)
+                            {
+                                DeleteFromIndex = NextCustomAttributeIndex;
+                                DeleteToIndex = GetTextLineIndex(NextCustomAttributeIndex, "nAttributes=", string.Empty);
+                                CleanUpLines(DeleteFromIndex, DeleteToIndex + 1);
+                            }
+                            else
+                            {
+                                DeleteFromIndex = NextCustomAttributeIndex;
+                                DeleteToIndex = GetTextLineIndex(NextCustomAttributeIndex, "nAttributes=", string.Empty);
+                                ChangeAttributesIndex(DeleteFromIndex, DeleteToIndex);
+                            }
+                        }
+                        else
+                        {
+                            DeleteFromIndex = NextCustomAttributeIndex;
+                            DeleteToIndex = GetTextLineIndex(NextCustomAttributeIndex, "nAttributes=", string.Empty);
+                            CleanUpLines(DeleteFromIndex, DeleteToIndex + 1);
+                        }
+                        NextCustomAttributeIndex = GetTextLineIndex(DeleteToIndex, "class CustomAttributes", string.Empty);
+                    }
+                    var NewRawTextLines = RAWTextLines.ToList();
+                    NewRawTextLines.RemoveAll(TextLine => TextLine == "-1");
+
+                    var saveSQMDialog = new SaveFileDialog();
+                    saveSQMDialog.CheckPathExists = true;
+                    saveSQMDialog.CheckFileExists = false;
+                    saveSQMDialog.OverwritePrompt = true;
+                    saveSQMDialog.ValidateNames = true;
+                    saveSQMDialog.Title = "Save SQM File:";
+                    saveSQMDialog.Filter = "SQM File|*.sqm";
+                    saveSQMDialog.InitialDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    var DialogResult = saveSQMDialog.ShowDialog(this);
+                    if (DialogResult == DialogResult.OK)
+                    {
+                        if (File.Exists(saveSQMDialog.FileName))
+                        {
+                            File.Delete(saveSQMDialog.FileName);
+                        }
+                        File.WriteAllLines(saveSQMDialog.FileName, NewRawTextLines);
+                        MessageBox.Show("Successful");
+                    }
+
+                    PathButton.Enabled = true;
+                    DeleteAllCheckBox.Enabled = true;
+                    CleanButton.Enabled = true;
+                    if (!DeleteAllCheckBox.Checked)
+                    {
+                        SimpleDeleteTextBox.Enabled = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("File doesn't exist anymore");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private bool ChangeAttributesIndex(int From, int To)
+        {
+            var AttributeCounter = 0;
+            for (int i = From; i <= To; i++)
+            {
+                if (RAWTextLines[i].ToLowerInvariant().Contains("class Attribute".ToLowerInvariant()))
+                {
+                    RAWTextLines[i] = RAWTextLines[i].Split(new string[1] { "class Attribute" }, StringSplitOptions.None)[0] + "class Attribute" + AttributeCounter++.ToString();
+                }
+                if (RAWTextLines[i].ToLowerInvariant().Contains("nAttributes=".ToLowerInvariant()))
+                {
+                    RAWTextLines[i] = RAWTextLines[i].Split(new string[1] { "nAttributes=" }, StringSplitOptions.None)[0] + "nAttributes=" + AttributeCounter.ToString() + ";";
+                }
+            }
+            return true;
+        }
+
+        private int GetTextLineIndex(int StartIndex, string SearchText, string BreakText)
+        {
+            for (int i = StartIndex; i < RAWTextLines.Length; i++)
+            {
+                if (RAWTextLines[i].ToLowerInvariant().Contains(SearchText.ToLowerInvariant()))
+                {
+                    return i;
+                }
+                else if (!BreakText.Equals(string.Empty) && RAWTextLines[i].ToLowerInvariant().Contains(BreakText.ToLowerInvariant()))
+                {
+                    return -1;
+                }
+            }
+            return -1;
+        }
+
+        private void CleanUpLines(int From, int To)
+        {
+            for (int i = From; i <= To; i++)
+            {
+                RAWTextLines[i] = "-1";
+            }
+        }
+
+        private void DeleteAllCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (DeleteAllCheckBox.Checked)
+            {
+                SimpleDeleteTextBox.Enabled = false;
+            }
+            else
+            {
+                SimpleDeleteTextBox.Enabled = true;
+            }
+        }
+    }
+}
